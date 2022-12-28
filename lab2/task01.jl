d0caf0cc384cb7596efd82c4e571140178ea6781
Arthur <37938234+supremestranger@users.noreply.github.com>
2022-12-28 21:20:00 +0300

    Update task01.jl

--- lab2/task01.jl
+++ lab2/task01.jl
@@ -1,9 +1,9 @@
 function main()
-    # добавить обращен ек  юзеру
-    data = [24, 26, 28, 32, 34, 36, 38, 40, 44, 46, 48, 52, 54, 57, 62, 63, 68, 70, 71, 72, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83]
+    println("Введите 30 чисел подряд через пробел.")
+    data = map(x -> parse(Int, x), split(readline()))
     left = 1
     right = 30
-    y = 36
+    y = data[rand(1:length(data))]
     found = -1
     while found != y
         idx = div(left + right, 2)
